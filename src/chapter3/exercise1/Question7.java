f0bbf36f6010a553a6141297be104203615a6537
Angshuman Das <angshu.imps@gmail.com>
2016-09-06 09:39:57 +0530

    Rewrite ackermann function

--- src/chapter3/exercise1/Question7.java
+++ src/chapter3/exercise1/Question7.java
@@ -3,18 +3,18 @@ package chapter3.exercise1;
 public class Question7 {
 
 	public static void main(String[] args) {
-		System.out.println(new Question7().ack(2, 2));
+		System.out.println(new Question7().ack(1, 3));
 	}
 
 	private int ack(int m, int n) {
 		if (m == 0) {
-			System.out.printf("m = %d, n = %d\n", m, n);
+			System.out.printf("check m is zero: m = %d, n = %d\n", m, n);
 			return n + 1;
 		} else if (m != 0 && n == 0) {
-			System.out.printf("m = %d, n = %d\n", m, n);
-			return ack(m - 1, n);
+			System.out.printf("check m is not zero and n is zero: m = %d, n = %d\n", m, n);
+			return ack(m - 1, 1);
 		} else {
-			System.out.printf("m = %d, n = %d\n", m, n);
+			System.out.printf("check m is not zero and n is not zero: m = %d, n = %d\n", m, n);
 			return ack(m - 1, ack(m, n - 1));
 		}
 	}
